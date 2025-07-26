4101f49a3acbcdc872c8af52c936cb43e31994db
Kirikuu <exito2712631@hotmail.com>
2025-07-25 19:15:52 -0500

    Creacion de Cursos

---
+++ models/Courses.js
@@ -0,0 +1,26 @@
+const { DataTypes}= require('sequelize');
+const sequelize =require('../config/database');
+
+const Courses = sequelize.define('Courses', {
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    area:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    duration:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    quotas:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    groups:{
+        type:DataTypes.STRING,
+        allowNull:false
+    }
+});
+module.exports = courses;
