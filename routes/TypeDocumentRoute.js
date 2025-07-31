15b7122dee552bcf3623ae0996e784274b8a5e27
krlfgr <karoldhn05@gmail.com>
2025-07-30 19:52:27 -0500

    configuracion ruta

--- routes/TypeDocumentRoute.js
+++ routes/TypeDocumentRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const UsuarioController = require('../controllers/TypeDocumentController')
+const TypeDocument = require('../controllers/TypeDocumentController')
 
-
+router.get("/asistencia/allTypeDocument",TypeDocument.getTypeDocument);
 
 module.exports=router;
