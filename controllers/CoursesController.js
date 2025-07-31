cff88047d5f795d58052f0fe82cff0bee8b442e3
Kirikuu <exito2712631@hotmail.com>
2025-07-30 19:32:45 -0500

    Ruta de cursos

--- controllers/CoursesController.js
+++ controllers/CoursesController.js
@@ -1,3 +1,14 @@
 const Courses=require('../models/Courses');
 
 
+exports.CoursesAssigned= async(req, res)=>{
+    try{ 
+    const courses = await Courses.findAll();
+    return res.json(courses);
+    }
+    catch(error){
+        return res.json(error)
+    }
+}
+    
+
